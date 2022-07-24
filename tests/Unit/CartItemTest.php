1519c89381680e5b914908ea9ed4e1c25461cc75
Dean Laverty <deannooel@gmail.com>
2022-07-24 21:38:18 +0100

    Added data provider for cart item unit test

---
+++ tests/Unit/CartItemTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Library\Cart\Item;
+use Library\Product\Product;
+use PHPUnit\Framework\TestCase;
+
+final class CartItemTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /**
+     * @dataProvider priceDataProvider
+     */
+    public function test_price_is_converted_to_pence_correctly($price, $discountedPrice): void
+    {
+        $product = new Product('P001', 'Photography', $price);
+        $item = new Item($product);
+
+        $this->assertSame($discountedPrice, $item->getPrice());
+    }
+
+    public function priceDataProvider(): array
+    {
+        return [
+            [
+                200.00,
+                20000,
+            ],
+            [
+                85.50,
+                8550,
+            ],
+            [
+                125.89,
+                12589,
+            ],
+        ];
+    }
+}
