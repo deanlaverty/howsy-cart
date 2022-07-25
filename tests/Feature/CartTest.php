05020a403eaa04670ac5ad0af687f77b07549ec2
Dean Laverty <deannooel@gmail.com>
2022-07-25 09:21:57 +0100

    Moved user creation into setup method

--- tests/Feature/CartTest.php
+++ tests/Feature/CartTest.php
@@ -21,40 +21,39 @@ use Psr\Container\ContainerInterface;
 
 final class CartTest extends TestCase
 {
-    protected Container $container;
+    private Container $container;
     private Cart $cart;
+    private User $user;
 
     public function setUp(): void
     {
         $this->container = require __DIR__ . '../../bootstrap.php';
         $this->cart = $this->container->get(Cart::class);
+        $this->user = new User(1, 'John Doe');
 
         parent::setUp();
     }
 
     public function test_cart_can_be_created_successfully(): void
     {
-        $user = new User(1, 'John Doe');
-        $cart = $this->cart->create($user);
+        $cart = $this->cart->create($this->user);
 
         $this->assertInstanceOf(Cart::class, $cart);
     }
 
     public function test_cart_throws_exception_when_created_twice(): void
     {
-        $user = new User(1, 'John Doe');
-        $cart = $this->cart->create($user);
+        $cart = $this->cart->create($this->user);
 
         $this->expectException(CartAlreadyExistsException::class);
         $this->expectErrorMessage('Cart already exists for user.');
 
-        $cart->create($user);
+        $cart->create($this->user);
     }
 
     public function test_cart_can_be_created_without_discount(): void
     {
-        $user = new User(1, 'John Doe');
-        $cart = $this->cart->create($user);
+        $cart = $this->cart->create($this->user);
 
         $this->assertEquals($cart->getDiscounts(), []);
     }
@@ -63,10 +62,9 @@ final class CartTest extends TestCase
     {
         $this->mockCartWithDiscount();
 
-        $user = new User(1, 'John Doe');
         $cart = $this->container->get(Cart::class);
 
-        $cart = $cart->create($user);
+        $cart = $cart->create($this->user);
 
         $discount = new Discount(
             Cart::USER_AGREED_CONTRACT_DISCOUNT,
@@ -79,8 +77,7 @@ final class CartTest extends TestCase
 
     public function test_cart_can_add_item(): void
     {
-        $user = new User(1, 'John Doe');
-        $cart = $this->cart->create($user);
+        $cart = $this->cart->create($this->user);
         $product = new Product('P001', 'Photography', 200.00);
 
         $cart->addItem($product->getCode());
@@ -91,8 +88,7 @@ final class CartTest extends TestCase
 
     public function test_cart_can_not_add_same_item_twice(): void
     {
-        $user = new User(1, 'John Doe');
-        $cart = $this->cart->create($user);
+        $cart = $this->cart->create($this->user);
         $product = new Product('P001', 'Photography', 200.00);
 
         $cart->addItem($product->getCode());
@@ -105,8 +101,7 @@ final class CartTest extends TestCase
 
     public function test_cart_can_not_add_invalid_item(): void
     {
-        $user = new User(1, 'John Doe');
-        $cart = $this->cart->create($user);
+        $cart = $this->cart->create($this->user);
         $product = new Product('INVALIDCODE', 'Photography', 200);
 
         $this->expectException(ProductNotFoundException::class);
@@ -117,8 +112,7 @@ final class CartTest extends TestCase
 
     public function test_cart_can_calculate_correct_total(): void
     {
-        $user = new User(1, 'John Doe');
-        $cart = $this->cart->create($user);
+        $cart = $this->cart->create($this->user);
         $product = new Product('P001', 'Photography', 200.00);
         $productTwo = new Product('P002', 'Floorplan', 100.00);
 
@@ -134,10 +128,9 @@ final class CartTest extends TestCase
     {
         $this->mockCartWithDiscount();
 
-        $user = new User(1, 'John Doe');
         $cart = $this->container->get(Cart::class);
 
-        $cart = $cart->create($user);
+        $cart = $cart->create($this->user);
 
         $discount = new Discount(
             Cart::USER_AGREED_CONTRACT_DISCOUNT,
@@ -164,13 +157,13 @@ final class CartTest extends TestCase
 
     private function mockCartWithDiscount()
     {
-        $userService = Mockery::mock(UserServiceInterface::class, function (MockInterface $mock) {
+        $this->userService = Mockery::mock(UserServiceInterface::class, function (MockInterface $mock) {
             $mock->shouldReceive('hasAgreedToContract')
                 ->once()
                 ->andReturn(true);
         })->makePartial();
 
-        $this->container->set(UserServiceInterface::class, $userService);
+        $this->container->set(UserServiceInterface::class, $this->userService);
 
         $this->container->set(Cart::class, function (ContainerInterface $c) {
             return new Cart(
